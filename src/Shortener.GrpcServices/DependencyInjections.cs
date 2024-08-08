599d3ac27e4f04fa5d06d39ae3062fa849ea8d28
Nabi Karampoor <thisisnabi@outlook.com>
2024-08-09 00:41:11 +0330

    Change style DependencyInjections

--- src/Shortener.GrpcServices/DependencyInjections.cs
+++ src/Shortener.GrpcServices/DependencyInjections.cs
@@ -6,9 +6,10 @@ public static class DependencyInjections
 {
     public static IServiceCollection AddShortenGrpcClient(this IServiceCollection services, string url)
     {
-        services.AddGrpcClient<ShortenUrl.ShortenUrlClient>(o =>    {
-            o.Address = new Uri(url);
+        services.AddGrpcClient<ShortenUrl.ShortenUrlClient>(options => {
+            options.Address = new Uri(url);
         });
+
         return services;
     }
 }
